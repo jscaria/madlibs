ced5cae34bcc65266c788fc477c96a60076467b4
Joel S <joelscaria@gmail.com>
2015-09-12 18:51:28 -0700

    Mad Lib - first version

--- MadLib.java
+++ MadLib.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class MadLib {
 
-   public MadLib(String filename) {
+   public static String noun = null;
+   public static String verb = null;
+   public static String adjective = null;
+   public static Scanner scanner = new Scanner(System.in);
    
+   public static void processVerb() {
+      verb = scanner.nextLine();
+   }
+   
+   public static void processNoun() {
+      noun = scanner.nextLine();
+   } 
+   
+   public static void processAdjective() {
+      adjective = scanner.nextLine();
+   }     
+   
+   public static void output() {
+      System.out.println("\nThe " + adjective + " teacher " + verb + " my " + noun + ", and I've never recovered.");
    }
 }
--- MyMadLib.java
+++ MyMadLib.java
@@ -1,23 +1,67 @@
-import java.util.*;
-
 public class MyMadLib {
    public static void main(String[] args) {
    
-      /** Text in orange in JGrasp like this are called comments.
-        * A comment is text that is written by someone like you
-        * that describes what the code is doing.
-        *
-        * For now, the comments are used to guide you on what to do.
-        * We will talk about comments in general in a future class. 
-        */
+      /* Text in orange in JGrasp like this are called comments.
+       * A comment is text that is written by someone like you
+       * that describes what the code is doing.
+       *
+       * For now, the comments are used to guide you on what to do.
+       * We will talk about comments in general in a future class. 
+       */
+        
+      /* 1. Write code that will output to the user "Hello world!" 
+       * 2. Replace the code outputting "Hello world!" with "Hello <your name>!"
+       */
+      System.out.println("Hello world!");
+       
       
-      /* Write code that will output to the user "Hello world!" */
-      System.out.println("I wrote some intro here!");
+      /*
+       * 3. Ask the user to pick a verb
+       */
+       
+      System.out.print("...");
+      
+      /*
+       * IGNORE THE CODE BELOW
+       */
+      MadLib.processVerb();
+      
+      
+      /*
+       * 4. Ask the user to pick a noun
+       */
+      System.out.print("...");
+      
+
+      /*
+       * IGNORE THE CODE BELOW
+       */       
+      MadLib.processNoun();
       
       
       
-      Scanner input = new Scanner(System.in);
-      MadLib madLib = new MadLib("fileToUse.txt");
-      MadLibReader runThis = new MadLibReader(madLib);     
+      /*
+       * 5. Ask the user to pick an adjective
+       */
+      System.out.print("...");
+       
+       
+      /*
+       * IGNORE THE CODE BELOW
+       */           
+      MadLib.processAdjective();
+      
+      
+      /*
+       * 6. Tell the user that you're going to output the MadLib
+       */      
+      System.out.print("...");
+      
+      
+      /*
+       * IGNORE THE CODE BELOW
+       */           
+      MadLib.output(); 
+        
    }
 }
